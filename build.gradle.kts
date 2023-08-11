3ae56fde9966630023bd0afbecd0ffa453a4d03e
Katherine Graf <katherinelgraf@gmail.com>
2023-08-11 12:14:36 -0500

    dependency cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -34,7 +32,6 @@ dependencies {
 	implementation ("com.google.code.gson:gson:2.10.1")
 	testImplementation("com.h2database:h2")
 	testImplementation(kotlin("test"))
-
 }
 
 tasks.withType<KotlinCompile> {
